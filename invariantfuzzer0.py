4e1311c474630d3898f2e46419e304e420729ec6
tizpaz <saeid.tizpazniari@colorado.edu>
2022-02-23 23:01:30 -0700

    update invariant

--- invariantfuzzer0.py
+++ invariantfuzzer0.py
@@ -1,12 +1,12 @@
 from z3 import *
 
 
-x, xp = Ints('x xp') 
+x, xp = Ints('x xp')
 
 P_given = lambda x: x == 0
 B_given = lambda x: x < 5
 T_given = lambda x, xp: xp == x + 1
-Q_given = lambda x: x == 5 
+Q_given = lambda x: x == 5
 
 K = 10
 
@@ -18,19 +18,20 @@ def Check(mkConstraints, I, P , B, T , Q):
     r = s.check()
     output = r.__repr__()
     if output == "sat":
-        #print("sat")
+        print("sat")
         return s.model()
     elif output == "unsat":
-        #print("unsat")
-        return {}
+        print("unsat")
+        return None
     else:
         print("Solver can't verify or disprove, it says: %s for invariant %s" %(r, I))
+        return None
 
-#Returns the conjunction of the CHC clauses of the system 
+#Returns the conjunction of the CHC clauses of the system
 def System(I, P , B, T , Q):
     # P(x) -> I(x)
     c1 = Implies(P(x), I(x))
-    # P(x) /\ B(x) /\ T(x,xp) -> I(xp) 
+    # P(x) /\ B(x) /\ T(x,xp) -> I(xp)
     c2 = Implies(And(B(x), I(x), T(x, xp)) , I(xp))
     # I(x) /\ ~B(x) -> Q(x)
     c3 = Implies(And(I(x), Not(B(x))), Q(x))
@@ -39,21 +40,36 @@ def System(I, P , B, T , Q):
 
 cex_List = []
 # Correct invariant is x <= 5
+#I_guess_predicate = x < 3
 I_guess = lambda x: x < 3
+# predicate should be the body of I_guess!
+predicate = x < 3
+
 
 for i in range(K):
     cex = Check(System, I_guess, P_given, B_given, T_given, Q_given)
     if cex is None:
         break
 
-    
-    # This is actual code, which gives same counterexamples after 3 different ones. (Actually after these it, there are no more - the issue is that Z3 still considers the system solvable?)
-    if( cex.evaluate(I_guess(x)) ):
-        I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(xp) )
+    if i == 0:
+        if( cex.evaluate(I_guess(x)) ):
+            I_guess = lambda x: Or(predicate, x == cex.evaluate(xp))
+        elif (cex.evaluate(I_guess(xp))):
+            I_guess = lambda x: Or(predicate, x == cex.evaluate(x))
     else:
-        I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(x))
-    
+        if( cex.evaluate(I_guess(x)) ):
+            I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(xp) )
+        else:
+            I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(x))
+
+    # This is actual code, which gives same counterexamples after 3 different ones. (Actually after these it, there are no more - the issue is that Z3 still considers the system solvable?)
+    # if( cex.evaluate(I_guess(x)) ):
+    #     I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(xp) )
+    # else:
+    #     I_guess = lambda t, old_I_guess=I_guess: Or(old_I_guess(t), t == cex.evaluate(x))
+
     cex_List.append(cex)
+    print(cex_List)
 
 
 # Print the list of counterexamples.
