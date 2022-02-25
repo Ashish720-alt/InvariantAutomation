28b163e4c032a6d4efe65791c8aaa21afdd6610c
Ashish <azk640@psu.edu>
2022-02-25 11:24:01 -0500

    Renamed the files based on which Z3's Lambda or Python's lambda used

--- testfile.py
+++ testfile.py
@@ -1,15 +1,14 @@
-from z3 import *
+# from z3 import *
 
+from pyeda.inter import *
 
-u, up, x = Ints('u up x') 
+# u, up, x = Ints('u up x') 
 
-# P = (x < 5)
+# P = Lambda( [x], x < 5 )
 
-# I = (x < 3)
+# I = Lambda ([x], x < 3)
 
-# C1 = Not(Implies(P, I ))
-
-# print(C1)
+# C1 = Not(Implies(P[u], I[u] ))
 
 # s = Solver()
 
@@ -17,12 +16,35 @@ u, up, x = Ints('u up x')
 
 # r = s.check()
 
-# print(s.model().eval(x))
-
 # print(r.__repr__())
 
-I = Lambda([x], x < 5)
+# s.add( Implies(P(u), u == 2) )
+
+# print(s.model().eval(x))
+
+# I = Lambda([x], x < 5)
 
 
+# print(J.body(), J.var_name[0] )
 
-print(J.body(), J.var_name[0] )
+# u, up, x, xp = Ints('u up x xp') 
+
+# I = Lambda([u], u < 3) 
+
+
+# def AndLambda (P, Q):
+#     return Lambda( [u],  And(P[u],Q[u]) )
+
+# print(simplify(AndLambda(I, Lambda([u], u == 3) )[x] ) )
+
+x, y = map(exprvar, 'xy')
+
+print((True and False) or not (False or True))
+
+list(iter_points([x, y]))
+
+X = exprvars('x', 3)
+f = truthtable(X, "00000001")
+print(f)
+
+print(truthtable2expr(f))
