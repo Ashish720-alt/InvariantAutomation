e7f1459d2e4ebbe270fbcf75ce5b093d8cf1931f
reatank <reatank@gmail.com>
2022-04-12 09:55:38 -0400

    TODO: rewrite Guess.mc_guess_inv

--- src/guess.py
+++ src/guess.py
@@ -11,6 +11,21 @@ class GuessStrategy(Enum):
     NEAR_CONSTANT = auto()
     OCTAGONAL_DOMAIN = auto()
     OCTAGONAL_DOMAIN_EXTENDED = auto()
+    MC_SMALL_CONSTANT = auto()
+    MC_NEAR_CONSTANT = auto()
+    MC_OCTAGONAL_DOMAIN = auto()
+    MC_OCTAGONAL_DOMAIN_EXTENDED = auto()
+
+    @staticmethod
+    def mc_to_not_mc(gs):
+        __mapping = {
+            GuessStrategy.MC_SMALL_CONSTANT: GuessStrategy.SMALL_CONSTANT,
+            GuessStrategy.MC_NEAR_CONSTANT: GuessStrategy.NEAR_CONSTANT,
+            GuessStrategy.MC_OCTAGONAL_DOMAIN: GuessStrategy.OCTAGONAL_DOMAIN,
+            GuessStrategy.MC_OCTAGONAL_DOMAIN_EXTENDED: GuessStrategy.OCTAGONAL_DOMAIN_EXTENDED,
+        }
+        return __mapping[gs]
+
 
 class Guess:
     def __init__(self, num_var, max_num_conj, max_num_disj, op_dist, strategy: GuessStrategy, **kwargs):
@@ -29,6 +44,10 @@ class Guess:
             OCTAGONAL_DOMAIN:
             OCTAGONAL_DOMAIN_EXTENDED:
                 consts: a list of constants.
+            MC versions additional parameters:
+                prev_I
+                change_size_prob
+                change_value_prob_ratio
         }
 
         Example:
@@ -43,16 +62,33 @@ class Guess:
         assert(len(self.op_dist) == len(conf.OP_DOMAIN))
 
         self.NUM_OCT_NONZERO_POS = 2
-        
+
         self.__guess = None
         if (strategy == GuessStrategy.SMALL_CONSTANT):
-            self.__guess = lambda: self.guess_inv_small_const(kwargs["max_const"])
+            self.__guess = lambda: self.guess_inv_small_const(
+                kwargs["max_const"])
         elif (strategy == GuessStrategy.NEAR_CONSTANT):
-            self.__guess = lambda: self.guess_inv_near_const(kwargs["consts"], kwargs["range"])
+            self.__guess = lambda: self.guess_inv_near_const(
+                kwargs["consts"], kwargs["range"])
         elif (strategy == GuessStrategy.OCTAGONAL_DOMAIN):
             self.__guess = lambda: self.guess_inv_oct(kwargs["consts"])
         elif (strategy == GuessStrategy.OCTAGONAL_DOMAIN_EXTENDED):
             self.__guess = lambda: self.guess_inv_oct_ext(kwargs["consts"])
+        elif (strategy == GuessStrategy.MC_SMALL_CONSTANT):
+            self.__guess = lambda: self.mc_guess_inv_small_const(
+                kwargs["max_const"], kwargs["prev_I"], kwargs["change_size_prob"], kwargs["change_value_prob_ratio"])
+        elif (strategy == GuessStrategy.MC_NEAR_CONSTANT):
+            self.__guess = lambda: self.mc_guess_inv_near_const(
+                kwargs["consts"], kwargs["range"], kwargs["prev_I"], kwargs["change_size_prob"], kwargs["change_value_prob_ratio"])
+        elif (strategy == GuessStrategy.MC_OCTAGONAL_DOMAIN):
+            self.__guess = lambda: self.mc_guess_inv_oct(
+                kwargs["consts"], kwargs["prev_I"], kwargs["change_size_prob"], kwargs["change_value_prob_ratio"])
+        elif (strategy == GuessStrategy.MC_OCTAGONAL_DOMAIN_EXTENDED):
+            self.__guess = lambda: self.mc_guess_inv_oct_ext(
+                kwargs["consts"], kwargs["prev_I"], kwargs["change_size_prob"], kwargs["change_value_prob_ratio"])
+        else:
+            raise Exception(
+                "Guess.__init__: unexpected guess strategy &s." % (strategy))
 
     def guess(self):
         return self.__guess()
@@ -98,7 +134,7 @@ class Guess:
     #         return conjunctive_clause
     #     return self.size_norm_conj(np.append(conjunctive_clause, np.array([0, 0, 0, -1, 0], ndmin=2), axis=0), max_num_conj)
 
-    def guess_DNF(self, coeff_dom, const_dom, is_oct_pred: bool,
+    def guess_inv(self, coeff_dom, const_dom, is_oct_pred: bool,
                   max_num_disj=None, max_num_conj=None):
         if (max_num_disj is None):
             max_num_disj = self.max_num_disj
@@ -112,48 +148,47 @@ class Guess:
 
     def guess_inv_small_const(self, max_const):
         const_dom = range(-max_const, max_const + 1)
-        return self.guess_DNF(const_dom, const_dom, False)
+        return self.guess_inv(const_dom, const_dom, False)
 
     def guess_inv_oct(self, consts: List[int]):
-        return self.guess_DNF([-1, 0, 1], consts, True)
+        return self.guess_inv([-1, 0, 1], consts, True)
 
     def guess_inv_oct_ext(self, consts: List[int]):
-        return self.guess_DNF([-1, 0, 1], consts, False)
+        return self.guess_inv([-1, 0, 1], consts, False)
 
     def guess_inv_near_const(self, values_in_program, k):
         val_set = set()
         for value in values_in_program:
             val_set |= set(range(value - k, value + k + 1))
         val_list = list(val_set)
-        return self.guess_DNF(val_list, val_list, False)
-        
-    def get_GP_list(self, a, r, N):
-        rv = []
-        curr = 1.0
-        for i in range(1, N + 1):
-            if (i == 1):
-                curr = a
+        return self.guess_inv(val_list, val_list, False)
+
+    @staticmethod
+    def get_geo_prob_list(domainList, centre, r):
+        # This function assumes that domainList is sorted, and elements cannot be repeated
+
+        def norm_neg_error_in_list(approx_list, neg_err):
+            # Assumes negative_error is a negative value
+            l = approx_list.copy()
+            i = 0
+            while i < len(l):
+                if (neg_err >= 0):
+                    break
+                l[i] = max(0, l[i] + neg_err)
+                neg_err += approx_list[i]
+                i += 1
             else:
-                curr = curr * r
-            rv = rv + [curr]
-        return rv
+                if(conf.DISPLAY_WARNINGS == conf.ON):
+                    print("Too much negative error in generating this list!")
+                l.append(neg_err)
+            return l
 
-    # print(get_GP_list(0.2,0.5,10))
+        def get_GP_list(a, r, N):
+            ret = [a]
+            for i in range(N-1):
+                ret.append(ret[-1]*r)
+            return ret
 
-    # Assumes negative_error is a negative value
-
-    def __norm_neg_error_in_list(self, approximated_list, negative_error):
-        if (negative_error >= 0):
-            return approximated_list
-        elif (approximated_list == []):
-            if(conf.DISPLAY_WARNINGS == conf.ON):
-                print("Too much negative error in generating this list!")
-            return [negative_error]
-        return [max(0, approximated_list[0] + negative_error)] + self.__norm_neg_error_in_list(approximated_list[1:], negative_error + approximated_list[0])
-
-    # This function assumes that domainList is sorted, and elements cannot be repeated
-
-    def __get_geo_prob_list(self, domainList, centre, r):
         index = domainList.index(centre)
         s1 = index
         s2 = len(domainList) - index - 1
@@ -164,70 +199,68 @@ class Guess:
         if (s1 == 0):
             a = ((float)(1 - r)) / (1 - r**s2)
             sublist1 = []
-            sublist2 = self.__get_GP_list(a, r, s2)
+            sublist2 = get_GP_list(a, r, s2)
         elif (s2 == 0):
             a = ((float)(1 - r)) / (1 - r**s1)
-            sublist1 = self.__get_GP_list(a, r, s1)[::-1]
-            sublist2 = self.__get_GP_list(a, r, s2)
+            sublist1 = get_GP_list(a, r, s1)[::-1]
+            sublist2 = get_GP_list(a, r, s2)
         else:
             a = ((float)(1 - r)**2) / (1 - r**s1) * (1 - r**s2)
-            sublist1 = self.__get_GP_list(a, r, s1)[::-1]
-            sublist2 = self.__get_GP_list(a, r, s2)
+            sublist1 = get_GP_list(a, r, s1)[::-1]
+            sublist2 = get_GP_list(a, r, s2)
 
         probList = sublist1 + [0] + sublist2
 
         computation_error = 1 - sum(probList)
         if(computation_error >= 0):
             if (index == 0):
-                probList[index + 1] = probList[index +
-                                               1] + (computation_error)
+                probList[index + 1] = probList[index + 1] \
+                    + (computation_error)
             elif (index == len(probList) - 1):
-                probList[index - 1] = probList[index -
-                                               1] + (computation_error)
+                probList[index - 1] = probList[index - 1] \
+                    + (computation_error)
             else:
-                probList[index - 1] = probList[index -
-                                               1] + (computation_error * 0.5)
-                probList[index + 1] = probList[index +
-                                               1] + (computation_error * 0.5)
+                probList[index - 1] = probList[index - 1] \
+                    + (computation_error * 0.5)
+                probList[index + 1] = probList[index + 1] \
+                    + (computation_error * 0.5)
         else:
-            probList = self.__norm_neg_error_in_list(probList[0:index], 0.5 * computation_error) + probList[index: index + 1] + (
-                self.__norm_neg_error_in_list((probList[index + 1:])[::-1], 0.5 * computation_error))[::-1]
+            probList = norm_neg_error_in_list(probList[0:index], 0.5 * computation_error) + probList[index: index + 1] + (
+                norm_neg_error_in_list((probList[index + 1:])[::-1], 0.5 * computation_error))[::-1]
         return probList
 
     # Implement the is_oct_pred version too!
-    # Assumes I_prev has correct numpy dimensions.
+    # Assumes prev_I has correct numpy dimensions.
 
-    def MC_guess_inv(self, I_prev, coeff_dom, const_dom, max_num_disj,
-                     change_size_prob, change_value_prob_GPratio, is_oct_pred):
+    def mc_guess_inv(self, coeff_dom, const_dom, is_oct_pred,
+                     prev_I, change_size_prob, change_value_prob_ratio):
         # Need sorted lists
         coeff_dom.sort()
-        conf.OP_DOMAIN = [-2, -1, 0, 1, 2]
         const_dom.sort()
 
-        prev_num_disj = I_prev.shape[0]
+        prev_num_disj = prev_I.shape[0]
 
-        if(np.random.choice([0, 1], p=np.array([1 - change_size_prob, change_size_prob])) == 1):
-            domain_of_disjunct_values = (
-                list((range(1, max_num_disj + 1))))
+        if np.random.rand() <= change_size_prob:
+            domain_of_disjunct_values = list(range(1, self.max_num_disj + 1))
             domain_of_disjunct_values.remove(prev_num_disj)
             if not domain_of_disjunct_values:
                 if (self.DISPLAY_WARNINGS == self.ON):
                     print(
                         "Size change not possible becuase domain of disjunct values is empty,")
-                return I_prev
+                return prev_I
             new_num_disj = np.random.choice(
                 domain_of_disjunct_values)
             if (new_num_disj < prev_num_disj):
-                return I_prev[0:new_num_disj]
+                return prev_I[0:new_num_disj]
             else:
                 additional_disjuncts = new_num_disj - prev_num_disj
-                max_num_conj = I_prev.shape[1]
-                return np.append(I_prev, self.guess_DNF(coeff_dom, const_dom, is_oct_pred, max_num_conj,
+                max_num_conj = prev_I.shape[1]
+                return np.append(prev_I, self.guess_inv(coeff_dom, const_dom, is_oct_pred, max_num_conj,
                                                         additional_disjuncts), axis=0)
         else:  # size is constant:
             prev_CC_position = np.random.choice(
                 range(0, prev_num_disj))
-            prev_CC = I_prev[prev_CC_position]
+            prev_CC = prev_I[prev_CC_position]
             prev_CC_size = prev_CC.shape[0]
             prev_P_position = np.random.choice(range(0, prev_CC_size))
             prev_P = prev_CC[prev_P_position]
@@ -240,15 +273,16 @@ class Guess:
                     domain = conf.OP_DOMAIN
                 else:
                     domain = const_dom
-                prob_list = self.__get_geo_prob_list(
-                    domain, prev_P[index_to_change], change_value_prob_GPratio)
+                prob_list = Guess.get_geo_prob_list(
+                    domain, prev_P[index_to_change], change_value_prob_ratio)
+                print(domain, prob_list)
                 newval = np.random.choice(domain, p=prob_list)
                 new_P[index_to_change] = newval
             else:
                 if (index_to_change < self.num_var):
                     domain = coeff_dom
-                    prob_list = self.__get_geo_prob_list(
-                        domain, prev_P[index_to_change], change_value_prob_GPratio)
+                    prob_list = Guess.get_geo_prob_list(
+                        domain, prev_P[index_to_change], change_value_prob_ratio)
                     newval = np.random.choice(domain, p=prob_list)
                     new_P[index_to_change] = newval
                     nonzero_index = (np.transpose(
@@ -261,45 +295,42 @@ class Guess:
                         domain = conf.OP_DOMAIN
                     else:
                         domain = const_dom
-                    prob_list = self.__get_geo_prob_list(
-                        domain, prev_P[index_to_change], change_value_prob_GPratio)
+                    prob_list = Guess.get_geo_prob_list(
+                        domain, prev_P[index_to_change], change_value_prob_ratio)
                     newval = np.random.choice(domain, p=prob_list)
                     new_P[index_to_change] = newval
             new_CC = prev_CC
             new_CC[prev_P_position] = new_P
-            I = I_prev
+            I = prev_I
             I[prev_CC_position] = new_CC
             return I
 
+    def mc_guess_inv_small_const(self, max_const, prev_I, change_size_prob, change_value_prob_ratio):
+        const_dom = list(range(-max_const, max_const + 1))
+        return self.mc_guess_inv(const_dom, const_dom, False,
+                                 prev_I, change_size_prob, change_value_prob_ratio)
 
-    def MC_guess_inv_small_const(self, I_prev, max_const, max_num_disj, change_size_prob, change_value_prob_GPratio):
-        const_dom = list(
-            range(-max_const, max_const + 1))
-        return self.MC_guess_inv(I_prev, const_dom, const_dom, max_num_disj, change_size_prob,
-                                 change_value_prob_GPratio, False)
+    def mc_guess_inv_oct(self, consts: List[int], prev_I, change_size_prob, change_value_prob_ratio):
+        return self.mc_guess_inv([-1, 0, 1], consts, True,
+                                 prev_I, change_size_prob, change_value_prob_ratio)
 
-    def MC_guess_inv_oct(self, I_prev, consts: List[int], max_num_disj, change_size_prob, change_value_prob_GPratio):
-        return self.MC_guess_inv(I_prev, [-1, 0, 1], consts, max_num_disj, change_size_prob,
-                                 change_value_prob_GPratio, True)
+    def mc_guess_inv_oct_ext(self, consts: List[int], prev_I, change_size_prob, change_value_prob_ratio):
+        return self.mc_guess_inv([-1, 0, 1], consts, True,
+                                 prev_I, change_size_prob, change_value_prob_ratio)
 
-    def MC_guess_inv_oct_ext(self, I_prev, consts: List[int], max_num_disj, change_size_prob, change_value_prob_GPratio):
-        return self.MC_guess_inv(I_prev, [-1, 0, 1], consts, max_num_disj, change_size_prob,
-                                 change_value_prob_GPratio, False)
-
-    def MC_guess_inv_near_const(self, I_prev, values_in_program, k, max_num_disj, change_size_prob, change_value_prob_GPratio):
-        list_of_values = []
+    def mc_guess_inv_near_const(self, values_in_program, k, prev_I, change_size_prob, change_value_prob_ratio):
+        val_set = set()
         for value in values_in_program:
-            list_of_values = list_of_values + \
-                list(range(value - k, value + k + 1))
-        list_of_values = list(set(list_of_values))  # remove duplicates
-        return self.MC_guess_inv(I_prev, list_of_values, list_of_values, max_num_disj, change_size_prob,
-                                 change_value_prob_GPratio, False)
+            val_set |= set(range(value - k, value + k + 1))
+        val_list = list(val_set)
+        return self.mc_guess_inv(val_list, val_list, False,
+                                 prev_I, change_size_prob, change_value_prob_ratio)
 
     # print(random_guess_predicate([-1,1,0] , programConstants, np.array([0.2, 0.2, 0.2, 0.2, 0.2])) )
     # print(random_guess_octagonalpredicate([-1,1,0] , programConstants, np.array([0.2, 0.2, 0.2, 0.2, 0.2])) )
     # print(random_guess_conjunctive_clause([-1,1,0] , programConstants, 3,  np.array([0.2, 0.2, 0.2, 0.2, 0.2]), 0) )
     # print(size_normalize_conjunctive_clause(random_guess_conjunctive_clause([-1,1,0] , programConstants, 3,  np.array([0.2, 0.2, 0.2, 0.2, 0.2]), 1), 5 ) )
-    # print( random_guess_DNF([-1,1,0] , programConstants, 3, 4, np.array([0.2, 0.2, 0.2, 0.2, 0.2]), 1) )
+    # print( random_guess_inv([-1,1,0] , programConstants, 3, 4, np.array([0.2, 0.2, 0.2, 0.2, 0.2]), 1) )
     # print( random_guess_invariant([-1,1,0] , programConstants, 3, 4, np.array([0.2, 0.2, 0.2, 0.2, 0.2]), 1) )
     # print(guess_invariant_smallConstants(10, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) ))
     # print(guess_invariant_octagonaldomain(programConstants, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) ))
@@ -309,20 +340,20 @@ class Guess:
     # Testing the MC functions:
     # I_p = guess_invariant_octagonaldomain(programConstants, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) )
     # print(I_p)
-    # print(MC_guess_invariant(I_p, [-1,0,1], programConstants, 3, 0.1, 0.5, 1, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) ))
+    # print(mc_guess_invariant(I_p, [-1,0,1], programConstants, 3, 0.1, 0.5, 1, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) ))
 
     # I_1 = guess_invariant_smallConstants(10, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) )
     # print(I_1)
-    # print(MC_guess_invariant_smallConstants (I_1, 10, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
+    # print(mc_guess_invariant_smallConstants (I_1, 10, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
 
     # I_2 = guess_invariant_octagonaldomain(programConstants, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) )
     # print(I_2)
-    # print(MC_guess_invariant_octagonaldomain (I_2, programConstants, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
+    # print(mc_guess_invariant_octagonaldomain (I_2, programConstants, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
 
     # I_3 = guess_invariant_octagonaldomain_extended(programConstants, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) )
     # print(I_3)
-    # print(MC_guess_invariant_octagonaldomain_extended (I_3, programConstants, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
+    # print(mc_guess_invariant_octagonaldomain_extended (I_3, programConstants, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
 
     # I_4 = guess_invariant_nearProgramConstants(programConstants, 5, 3, 3, np.array([0.2, 0.2, 0.2, 0.2, 0.2]) )
     # print(I_4)
-    # print(MC_guess_invariant_nearProgramConstants(I_4, programConstants, 5, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
+    # print(mc_guess_invariant_nearProgramConstants(I_4, programConstants, 5, 3, 0.1, 0.5, np.array([0.2, 0.2, 0.2, 0.2, 0.2])))
--- src/main.py
+++ src/main.py
@@ -13,12 +13,13 @@ import numpy as np
 """ Main function.
 """
 
-def guess_inv(repr: Repr, guess_strat, max_const=None, guess_range=None):
+
+def guess_inv(repr: Repr, max_guesses, guess_strat, max_const=None, guess_range=None):
     cost = float('inf')
     count_guess = 0
     num_var = repr.get_num_var()
-    I_arr = None
-    while (cost != 0 and count_guess < conf.max_guesses):
+    I = None
+    while (cost != 0 and count_guess < max_guesses):
         count_guess += 1
         guesser = Guess(num_var, conf.max_conjuncts, conf.max_disjuncts,
                         np.array([0.2, 0.2, 0.2, 0.2, 0.2]),
@@ -26,11 +27,56 @@ def guess_inv(repr: Repr, guess_strat, max_const=None, guess_range=None):
                         max_const=max_const,
                         range=guess_range,
                         consts=repr.get_consts())
-        I_arr = guesser.guess()  # I
-        cost = Cost(repr, I_arr).get_cost()
-        # print(cost1, cost2, cost3, end = '')
-        print('   ', round(cost, 2), '\n')
-    return (I_arr, cost)
+        I = guesser.guess()
+        cost = Cost(repr, I).get_cost()
+        print(count_guess, '   ', DNF_to_z3expr(I), "\t", end='')
+        print('   ', round(cost, 2))
+    return (I, cost)
+
+
+
+def mc_guess_inv(repr: Repr, max_guesses, guess_strat, max_const=None, guess_range=None, change_size_prob=None, change_value_prob_ratio=None):
+    count_guess = 0
+    cost = float('inf')
+    num_var = repr.get_num_var()
+    I = None
+    while (cost > 0 and count_guess < max_guesses):
+        count_guess += 1
+
+        if (count_guess == 1):
+            (I, cost) = guess_inv(repr, 1, GuessStrategy.mc_to_not_mc(guess_strat), max_const=max_const, guess_range=guess_range)
+            continue
+
+        prev_cost = cost
+        prev_I = I
+        guesser = Guess(num_var, conf.max_conjuncts, conf.max_disjuncts,
+                        np.array([0.2, 0.2, 0.2, 0.2, 0.2]),
+                        guess_strat,
+                        prev_I=prev_I,
+                        max_const=max_const,
+                        range=guess_range,
+                        consts=repr.get_consts(),
+                        change_size_prob=change_size_prob,
+                        change_value_prob_ratio=change_value_prob_ratio)
+        I = guesser.guess()  # I
+        cost = Cost(repr, I).get_cost()
+
+        if (cost > prev_cost):
+            cost = prev_cost
+            I = prev_I
+            continue
+
+        # 0.05 is added so that prob_of_change is not zero if curr_cost = prev_cost
+        change_prob = min(1.0 - (cost/prev_cost) + 0.05, 1.0)
+        # Don't change
+        if(np.random.rand() > change_prob):
+            cost = prev_cost
+            I = prev_I
+        else:
+            print(count_guess, '   ', DNF_to_z3expr(I), "\t", end='')
+            print('   ', round(cost, 2))
+
+    return
 
 
 guess_inv(
@@ -38,70 +84,18 @@ guess_inv(
               B=np.array([[[1, 0, 0, -2, 6]]]),
               Q=np.array([[[1, 0, 0, 0, 6]]]),
               T=np.array([[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])),
+    conf.max_guesses,
     GuessStrategy.OCTAGONAL_DOMAIN
 )
 
-
-# MC_invariant_guess(max_guesses, (3, 1), max_conjuncts, max_disjuncts, 0.1, 0.5)
-
-
-def MC_invariant_guess(guesses, guess_strat, num_conj, num_disj, k, r):
-    count_guess = 0
-    curr_cost = float('inf')
-    curr_I = np.empty(shape=(0, max_conjuncts, n+2))
-    while (curr_cost != 0 and count_guess < guesses):
-        count_guess = count_guess + 1
-        prev_cost = curr_cost
-        prev_I = curr_I
-        if (count_guess == 1):
-            rv = random_invariant_guess(1, guess_strat, num_conj, num_disj)
-            curr_cost = rv[1]
-            curr_I = rv[0]
-            continue
-        else:
-            if (guess_strat[0] == 1):
-                curr_I = MC_guess_inv_smallConstants(
-                    prev_I, guess_strat[1], num_disj, k, r, np.array([0.2, 0.2, 0.2, 0.2, 0.2]))
-            elif (guess_strat[0] == 2):
-                if (guess_strat[1] == 0):
-                    curr_I = MC_guess_inv_octagonaldomain(
-                        prev_I, programConstants, num_disj, k, r, np.array([0.2, 0.2, 0.2, 0.2, 0.2]))
-                else:
-                    curr_I = MC_guess_inv_octagonaldomain_extended(
-                        prev_I, programConstants, num_disj, k, r, np.array([0.2, 0.2, 0.2, 0.2, 0.2]))
-            elif (guess_strat[0] == 3):
-                curr_I = MC_guess_inv_nearProgramConstants(
-                    prev_I, programConstants, guess_strat[1], num_disj, k, r, np.array([0.2, 0.2, 0.2, 0.2, 0.2]))
-
-            curr_I_g = convert_DNF_to_lambda(curr_I)
-            C1_cexList = GenerateCexList_C1(curr_I_g, s)
-            # print(C1_cexList)
-            C2_cexList = GenerateCexList_C2(curr_I_g, s)
-            # print(C2_cexList)
-            C3_cexList = GenerateCexList_C3(curr_I_g, s)
-            # print(C3_cexList)
-
-            # Get costFunction
-            cost1 = J1(curr_I, C1_cexList)
-            cost2 = J2(C2_cexList)
-            cost3 = J3(Q_array, C3_cexList)
-            curr_cost = K1*cost1 + K2*cost2 + K3*cost3
-
-            if (curr_cost > prev_cost):
-                curr_cost = prev_cost
-                curr_I = prev_I
-                continue
-            else:
-                # 0.05 is added so that prob_of_change is not zero if curr_cost = prev_cost
-                prob_of_change = min(1.0 - (curr_cost/prev_cost) + 0.05, 1.0)
-                # Don't change
-                if(np.random.choice([0, 1], p=np.array([1 - prob_of_change, prob_of_change])) == 0):
-                    curr_cost = prev_cost
-                    curr_I = prev_I  # Isn't it pointer assignment here? Won't this give a really bad error, the same as the semantic bug as before?!?
-                else:
-                    print(count_guess, '   ', end='')
-                    print_DNF(curr_I, 0)
-                    print("\t", end='')
-                    # print(cost1, cost2, cost3, end = '')
-                    print('   ', round(curr_cost, 2), '\n')
-    return
+# mc_guess_inv(
+#     get_input(P=np.array([[[1, 0, 0, 0, 0]]]),
+#               B=np.array([[[1, 0, 0, -2, 6]]]),
+#               Q=np.array([[[1, 0, 0, 0, 6]]]),
+#               T=np.array([[1, 0, 0, 1], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])),
+#     conf.max_guesses,
+#     GuessStrategy.MC_NEAR_CONSTANT,
+#     guess_range=1,
+#     change_size_prob=0.1,
+#     change_value_prob_ratio=0.5
+# )
