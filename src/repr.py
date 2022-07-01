78f59e2613c46467e6bc29564310cfc06060a17c
Ashish <azk640@psu.edu>
2022-07-01 15:11:39 -0400

    Latest Changes

--- src/repr.py
+++ src/repr.py
@@ -1,8 +1,9 @@
 """ Representation of the logical system to be solved.
 """
 import numpy as np
-from dnf import DNF_to_z3expr, trans_func_to_z3expr
+from dnf import DNF_to_z3expr, trans_func_to_z3expr, dnfconjunction
 
+##################################################
 def I(n):
     return np.identity(n + 1, dtype=int)
 
@@ -10,6 +11,87 @@ def E(n, pos ): #Indices run from 1 to n+1
     T = np.zeros(shape=(n+1, n+1), dtype=int)
     T[pos[0]-1][pos[1]-1] = 1
     return T
+##################################################
+
+# A partialTransitionFuncPair is a pair(t_i, B_i /\ B) where T is a partial LI transition function, and B_i, B are dnfs
+class partialTransitionFuncPair:
+    def __init__(self, transition_matrix, DNF, B):
+        self.t = transition_matrix
+        self.b = dnfconjunction(DNF, B)
+
+def detTransitionFunc(*args, B):
+    return [partialTransitionFuncPair(x[0], x[1], B) for x in args]
+
+def nondetTransitionRel(*args, B):
+    return [partialTransitionFuncPair(x[0], x[1], B) for x in args]
+
+def genTransitionRel(Dtf, Ntr):
+    return [Dtf, Ntr]
+
+class Repr:
+    def __init__(self, P, B, T, Q):
+
+        self.n = len(P[0][0]) - 2  # n+1 is op, n+2 is const
+        
+        self.P = P.copy()
+        self.B = B.copy()
+        self.Q = Q.copy()
+        self.T = T.copy()
+        
+        self.P_z3expr = DNF_to_z3expr(P)
+        self.B_z3expr = DNF_to_z3expr(B)
+        self.Q_z3expr = DNF_to_z3expr(Q)
+        self.T_z3expr = trans_func_to_z3expr(self.T)
+
+        def _extract_pc():
+            def f(dnf): 
+                ret = set()
+                for cc in dnf: 
+                    ret = ret | set(cc.flatten()) 
+                return ret
+
+            ret = f(self.P) | f(self.B) | f(self.Q)
+            ptfp_list = self.T[0] + self.T[1] 
+            for ptfp in ptfp_list:
+                ret |= f(ptfp.b) |  set(ptfp.t.flatten()) 
+            return list(ret) 
+        
+        self.pc = _extract_pc()
+
+    def get_n(self):
+        return self.n
+
+    def get_pc(self):
+        return self.pc
+
+    def get_P(self):
+        return self.P
+
+    def get_B(self):
+        return self.B
+
+    def get_Q(self):
+        return self.Q
+
+    def get_T(self):
+        return self.T
+
+    def get_P_z3expr(self):
+        return self.P_z3expr
+
+    def get_B_z3expr(self):
+        return self.B_z3expr
+
+    def get_Q_z3expr(self):
+        return self.Q_z3expr
+
+    def get_T_z3expr(self):
+        return self.T_z3expr
+
+
+
+
+
 
 class PartialTransitionFunc:
     def __init__(self, DNF, transition_matrix):
