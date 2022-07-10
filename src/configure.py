317601ee3f362b5fe5200d64e6a0a79d9a2b6585
Ashish <azk640@psu.edu>
2022-07-10 12:05:26 -0400

    Latest Changes

--- src/configure.py
+++ src/configure.py
@@ -15,9 +15,9 @@ class Configure:
 
     p = 0.05
     
-    alpha = 1e+300
+    alpha = 1e+40
     gamma = 2.0 #Need floating point constant here
-
+    beta_0 = 100
 
     # Operators
     OP = {
@@ -33,4 +33,4 @@ class Configure:
     OFF = 0
     # Print iterations or not?
     PRINT_ITERATIONS = ON
-    PRINT_REJECT_ITERATIONS = OFF
+    PRINT_REJECT_ITERATIONS = ON
