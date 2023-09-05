5d9c8e7c09512ee579ef86e475a41d1bfce5e70e
Jialun Zhang <jjz5354@psu.edu>
2023-09-05 10:42:22 -0400

    fix a bug

--- rotation-graph/src/main.rs
+++ rotation-graph/src/main.rs
@@ -4,7 +4,7 @@ use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::Write;
 use std::sync::Arc;
-use std::thread;
+use std::{thread, vec};
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -24,7 +24,7 @@ struct Args {
     #[arg(short, long, default_value_t = 1)]
     p: usize,
 
-    #[arg(short, long, default_value_t = String::new("result.json"))]
+    #[arg(short, long)]
     file_name: String,
 }
 
@@ -35,7 +35,18 @@ fn generate_vectors(n: usize, k: i64) -> Vec<Vec<i64>> {
     let mut i;
 
     loop {
-        vectors.push(vector.clone());
+        // push if gcd(vector) == 1
+        let mut gcd = vector[0];
+        let mut flag = if vector[0] == 0 { true } else { false };
+        for i in 1..n {
+            gcd = num::integer::gcd(gcd, vector[i]);
+            if vector[i] != 0 {
+                flag = false;
+            }
+        }
+        if gcd == 1 && (!flag) {
+            vectors.push(vector.clone());
+        }
         i = 0;
         while i < n && vector[i] == k {
             vector[i] = -k;
@@ -53,34 +64,36 @@ fn generate_vectors(n: usize, k: i64) -> Vec<Vec<i64>> {
 // Given n and p, split the workload of half of the n*n matrix (half by diagonal)
 // into p parts, and return the indexes of the splitting rows
 fn split_half_matrix(n: usize, p: usize) -> Vec<usize> {
-    let mut indexes = vec![]; // indexes of the splitting rows
+    let mut indexes = vec![0]; // indexes of the splitting rows
     let len = n * (n + 1) / 2;
-    let avg = len / p;
+    let avg = (len as f64 / p as f64).ceil() as usize;
     let mut current_row = 0;
     let mut current_row_len = n;
-    let mut flag = p;
-    for i in 0..p {
-        indexes.push(current_row);
+
+    for _ in 0..p {
+        if current_row >= n {
+            break;
+        }
+
         let mut current_sum = current_row_len;
         current_row += 1;
         current_row_len -= 1;
-        if current_row == n {
-            // You don't need that many processes
-            flag = i;
-            break;
-        }
-        let lim = avg * (i + 1);
-        while current_sum < lim {
+        while current_sum < avg {
+            if current_row >= n {
+                break;
+            }
             current_sum += current_row_len;
             current_row += 1;
             current_row_len -= 1;
         }
+        indexes.push(current_row);
     }
-    for _ in flag..p + 1 {
+    let len = indexes.len();
+    for _ in len..p + 1 {
         indexes.push(n);
     }
     indexes
-}
+} 
 
 // helper function, convert a vector of tuples to a hashmap
 fn vec_to_map(v: &Vec<(usize, usize)>) -> HashMap<usize, HashSet<usize>> {
@@ -99,23 +112,29 @@ fn index_to_vec(vector: &Vec<Vec<i64>>, index: usize) -> Vec<i64> {
 }
 
 #[allow(dead_code)]
-fn output_to_file(v: &Vec<(usize, usize)>, file_path: &str) {
+fn output_to_file(v: &Vec<(usize, usize)>, file_path: &String) {
     let hashmap = vec_to_map(v);
     let serialized = serde_json::to_string(&hashmap).expect("Serialization failed");
-    
+
     let mut file = File::create(file_path).expect("File creation failed");
     file.write_all(serialized.as_bytes()).expect("Write failed");
 }
 
 // output to a file in dict format
-fn output_to_file_readable(v: &Vec<(usize, usize)>, vector: &Vec<Vec<i64>>, file_path: &str) {
+fn output_to_file_readable(v: &Vec<(usize, usize)>, vector: &Vec<Vec<i64>>, file_path: &String) {
     let hashmap = vec_to_map(v);
-    let hashmap: HashMap<String, Vec<Vec<i64>>> = hashmap.iter().map(|(key, value)| {
-        (
-            format!("{:?}",index_to_vec(vector, *key)),
-            value.iter().map(|x| index_to_vec(vector, *x)).collect::<Vec<_>>(),
-        )
-    }).collect();
+    let hashmap: HashMap<String, Vec<Vec<i64>>> = hashmap
+        .iter()
+        .map(|(key, value)| {
+            (
+                format!("{:?}", index_to_vec(vector, *key)),
+                value
+                    .iter()
+                    .map(|x| index_to_vec(vector, *x))
+                    .collect::<Vec<_>>(),
+            )
+        })
+        .collect();
 
     let mut file = File::create(file_path).expect("File creation failed");
     let serialized = serde_json::to_string_pretty(&hashmap).expect("Serialization failed");
@@ -124,7 +143,7 @@ fn output_to_file_readable(v: &Vec<(usize, usize)>, vector: &Vec<Vec<i64>>, file
 
 fn main() {
     let args = Args::parse();
-    let (n, k, p, t) = (args.n, args.k, args.p, args.t);
+    let (n, k, p, t, file_name) = (args.n, args.k, args.p, args.t, args.file_name);
     let pb = Arc::new(ProgressBar::new(p as u64));
     pb.set_style(
         ProgressStyle::default_bar()
@@ -183,5 +202,47 @@ fn main() {
     }
     pb.finish();
 
-    output_to_file_readable(&result, &vectors, "result.txt");
+    output_to_file_readable(&result, &vectors, &file_name);
+}
+
+mod test {
+    #[test]
+    fn test_split_half_matrix() {
+        fn check_valid_indexes(p: usize, n: usize, v: &Vec<usize>) -> bool {
+            let nn = n;
+            let n = n * (n + 1) / 2;
+            let avg: usize = n / p;
+            if v.len() != p + 1 {
+                println!("1:{:?} {} {}", v, p, n);
+                return false;
+            }
+            if v[0] != 0 {
+                println!("2:{:?} {} {}", v, p, n);
+                return false;
+            }
+            if v[p] != nn {
+                println!("3:{:?} nn:{}, p:{} n:{} avg:{}", v, nn, p, n, avg);
+                return false;
+            }
+            for i in 0..p {
+                if v[i] > v[i + 1] {
+                    println!("1:{:?} {} {} {}", v, v[i], v[i + 1], n);
+                    return false;
+                }
+                if (nn - (v[i + 1] - 1) + nn - v[i]) * (v[i + 1] - v[i]) / 2 > avg + nn {
+                    println!("2:{:?} {} {} {}, {}, nn: {}", v, v[i], v[i + 1], n, avg, nn);
+                    return false;
+                }
+            }
+
+            true
+        }
+        // Here n is the len of generated vectors, p is the number of processes
+        for n in 1..25 * 25 * 25 * 25 * 25 {
+            for p in 1..48 {
+                let v = super::split_half_matrix(n, p);
+                assert!(check_valid_indexes(p, n, &v));
+            }
+        }
+    }
 }
