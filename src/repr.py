29cb2d648d3b895e7394c703efd6d7a28ff259b7
Ashish <azk640@psu.edu>
2024-06-07 00:50:25 -0400

    Added locks for printing in filemode

--- src/repr.py
+++ src/repr.py
@@ -30,7 +30,7 @@ def genLItransitionrel(B, *args):
 class Repr:
     def __init__(self, P, B, T, Q, Var, c, d, clist = []):
         
-        self.n = len(P[0][0]) - 2  # n+1 is op, n+2 is const
+        self.n = len(Var)  
          
         if (conf.NONITERATIVE_PRECONDITION == conf.ON):
             self.P = getIterativeP(P, B)
